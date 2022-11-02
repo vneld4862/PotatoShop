fef37a1a5491dedbcfc119c0e30edb772f9c5914
vneld4862 <vneld4862@gmail.com>
2022-11-02 12:31:53 +0900

    프로젝트생성

---
+++ src/main/java/kh/study/team2/PotatoShopApplication.java
@@ -0,0 +1,13 @@
+package kh.study.team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotatoShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kh/study/team2/PotatoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.study.team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
