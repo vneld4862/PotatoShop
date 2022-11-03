eef88d91ff3ff6205341df2fb32e223a1db5db61
zeejeezee <jeezeedev@gmail.com>
2022-11-03 13:35:08 +0900

    admin 폴더, admin_layout, admin_menu, 회원관리 페이지 생성

--- src/main/java/kh/study/team2/shop/admin/controller/AdminController.java
+++ src/main/java/kh/study/team2/shop/admin/controller/AdminController.java
@@ -1,10 +1,16 @@
 package kh.study.team2.shop.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	@GetMapping("/memberManage")
+	public String memberManage() {
+		
+		return "content/admin/member_manage";
+	}
 
 }
