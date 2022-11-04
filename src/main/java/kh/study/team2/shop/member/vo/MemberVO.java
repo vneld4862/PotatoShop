e45bd25be46870f612093208ae5ce0673ca12111
miinha <alsgk27@gmail.com>
2022-11-04 15:47:13 +0900

    memberVO 수정

--- src/main/java/kh/study/team2/shop/member/vo/MemberVO.java
+++ src/main/java/kh/study/team2/shop/member/vo/MemberVO.java
@@ -1,5 +1,8 @@
 package kh.study.team2.shop.member.vo;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import groovy.transform.ToString;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,15 +11,37 @@ import lombok.Setter;
 @Getter
 @Setter
 public class MemberVO {
+	@NotBlank(message = "아이디는 필수입력입니다.")
 	 private String memberId;
+	@NotBlank(message = "비밀번호는 필수입력입니다.")
 	 private String memberPw;
-	 private String	meberName;
+	@NotBlank(message = "이름은 필수입력입니다.")
+	 private String	memberName;
+	@NotBlank(message = "닉네임은 필수입력입니다.")
 	 private String memberNickName;
+	@NotBlank(message = "연락처는 필수입력입니다.")
+	@Size(min = 10, max = 11)
 	 private String memberTell;
+	@NotBlank(message = "주소는 필수입력입니다.")
 	 private String memberAddr;
 	 private String addrDetail;
 	 private String memberEmail;
 	 private String memberRole;
 	 private String memberStatus;
 	 private String regDate; 
+	 private String[] memberTells;
+	 
+	 public String getMemberTell() {
+		 if(memberTells == null) {
+			 return null;
+		 }
+		 else {
+			 String result = "";
+			 
+			 for(String tell : memberTells) {
+				 result += tell;
+			 }
+			 return result;
+		 }
+	 }
 }
