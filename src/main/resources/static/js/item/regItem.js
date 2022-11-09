d7d0702e3a7eaa101ad32d9c77e505ee3689c12d
jinyeee <wls9546@naver.com>
2022-11-09 11:03:56 +0900

    상품등록기능 추가구현(거래방법,거래주소 추가)

--- src/main/resources/static/js/item/regItem.js
+++ src/main/resources/static/js/item/regItem.js
@@ -5,7 +5,7 @@ function searchTrAddr(){
 		        oncomplete: function(data) {
 		            //도로명 주소
 		        	 const roadAddr = data.roadAddress; 
-		             document.querySelector('#memberAddr').value = roadAddr;//내가 선택한 input태그에 밸류값으로 도로명주소를 넣어준다.
+		             document.querySelector('#tradeAddr').value = roadAddr;//내가 선택한 input태그에 밸류값으로 도로명주소를 넣어준다.
 		             
 		        }
 		    }).open();
