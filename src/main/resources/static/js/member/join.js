fd54f56f20a663310c8177d5d12ead203562fc3c
miinha <alsgk27@gmail.com>
2022-11-10 09:59:15 +0900

    회원가입 중복확인 기능 수정 완료

--- src/main/resources/static/js/member/join.js
+++ src/main/resources/static/js/member/join.js
@@ -11,33 +11,35 @@ function searchAddr(){
 //중복확인 버튼 클릭시 진행
 function check(){
 	const member_id = $("#memberId").val();
+	if(member_id == "" || member_id.length < 2){
+    		$(".successNameChk").text("이름은 2자 이상 8자 이하로 설정해주세요 :)");
+			$(".successNameChk").css("color", "red");
+	}else{
 	
-	//ajax start
-	$.ajax({
-		url: '/member/checkDuplId', //요청경로
-		type: 'post',
-		data: {'memberId':member_id}, //필요한 데이터
-		success: function(result) {
-			if(result){
-				alert('사용가능한 아이디입니다.');
+		//ajax start
+		$.ajax({
+			url: '/member/checkDuplId', //요청경로
+			type: 'post',
+			data: {'memberId':member_id}, //필요한 데이터
+			success: function(result) {
+				if(result){
+					$(".successNameChk").text(" 사용가능한 아이디입니다.");
+					$(".successNameChk").css("color", "green");
+				}
+				else{
+					$(".successNameChk").text(" 이미 사용중인 아이디입니다.");
+					$(".successNameChk").css("color", "red");
+				}
+			},
+			error: function() {
+				alert('로그인 실패');
 			}
-			else{
-				alert('이미 사용중인 아이디가 존재합니다');
-			}
-		},
-		error: function() {
-			alert('로그인 실패');
-		}
-	});
-	//ajax end
+		});
+		//ajax end
 }
 
-//join 버튼 클릭시 진행
-function join(){
-	
-	
-}
 
+}
 
 
 		
