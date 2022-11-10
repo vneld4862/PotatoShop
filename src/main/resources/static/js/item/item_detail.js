17b16b1ef9c2eeff13618a75ad862f15b7084d57
jinyeee <wls9546@naver.com>
2022-11-10 20:58:50 +0900

    찜 기능 추가구현 상세정보화면 추가구현

--- src/main/resources/static/js/item/item_detail.js
+++ src/main/resources/static/js/item/item_detail.js
@@ -1,19 +1,19 @@
-alert('상품 상세화면입니당~ 히히');
+//alert('상품 상세화면입니당~ 히히');
 
 function insertWish(wishBtn){
-	alert('찜꽁');
+//	alert('찜꽁');
 	const itemCode = wishBtn.dataset.itemName;
-	alert(itemCode);
+//	alert(itemCode);
 	
-	const itemCode2 = document.querySelector('#itemCodeInput').value;
-	alert(itemCode2);
+//	const itemCode2 = document.querySelector('#itemCodeInput').value;
+//	alert(itemCode2);
 	//ajax start
 	$.ajax({
 	   url: '/item/insertWish', //요청경로
 	    type: 'post',
-	    data:{'itemCode': itemCode2}, //필요한 데이터
+	    data:{'itemCode': itemCode}, //필요한 데이터
 	    success: function(result) {
-	      alert('aaa');
+	      alert('해당 상품을 찜했습니다.');
 	    },
 	    error: function(){
 	       alert('실패');
