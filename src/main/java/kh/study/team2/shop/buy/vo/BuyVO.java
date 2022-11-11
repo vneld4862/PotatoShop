de456492f746c5d69c8c02991e996f2560a67e7c
miinha <alsgk27@gmail.com>
2022-11-11 09:46:36 +0900

    BuyVO 수정

--- src/main/java/kh/study/team2/shop/buy/vo/BuyVO.java
+++ src/main/java/kh/study/team2/shop/buy/vo/BuyVO.java
@@ -1,5 +1,15 @@
 package kh.study.team2.shop.buy.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class BuyVO {
-	
+	private String buyCode;
+	private String memberId;
+	private String buyDate;
+	private String totalPrice;
 }
