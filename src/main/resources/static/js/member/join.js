bb00e811f0f26af9d47c4fdea8d2c5a89ca1bccf
miinha <alsgk27@gmail.com>
2022-11-11 17:37:12 +0900

    회원가입 휴대폰 인증 기능 수정 중

--- src/main/resources/static/js/member/join.js
+++ src/main/resources/static/js/member/join.js
@@ -32,7 +32,7 @@ function check(){
 				}
 			},
 			error: function() {
-				alert('로그인 실패');
+				alert('실패');
 			}
 		});
 		//ajax end
@@ -53,8 +53,29 @@ function passChk(){
 }
 
 
+function test(){
+	Swal.fire({
+	   title: '정말로 그렇게 하시겠습니까?',
+	   text: '다시 되돌릴 수 없습니다. 신중하세요.',
+	   icon: 'error',
+	   
+	   showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+	   confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
+	   cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+	   confirmButtonText: '승인', // confirm 버튼 텍스트 지정
+	   cancelButtonText: '취소', // cancel 버튼 텍스트 지정
+	   
+	   reverseButtons: false, // 버튼 순서 거꾸로
+   
+}).then(result => {
+   // 만약 Promise리턴을 받으면,
+   if (result.isConfirmed) { // 만약 모달창에서 confirm 버튼을 눌렀다면
+   
+      Swal.fire('승인이 완료되었습니다.', '화끈하시네요~!', 'success');
+   }
+});
 
-
+}
 
 
 		
