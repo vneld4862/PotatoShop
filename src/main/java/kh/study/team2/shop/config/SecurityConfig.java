7911f7ef76d1118062bacde904ef94ce1e921ab7
zeejeezee <jeezeedev@gmail.com>
2022-11-14 12:01:18 +0900

    스프링 시큐리티 기능 구현 / top.html, login.html, memmber-mapper.xml 수정 및 UserDetailsServiceImpl, login_result, SecurityConfig 추가

---
+++ src/main/java/kh/study/team2/shop/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package kh.study.team2.shop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+		security.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/item/list"
+							, "/member/join"
+							, "/member/login"
+							, "/item/itemDetail").permitAll() //회원가입, 로그인, 게시글 목록, 게시글 상세보기
+				.antMatchers("/admin/**").hasRole("ADMIN")
+				//.anyRequest().authenticated() //로그인한 유저는 접근 허용
+				//제일 마지막에 권한 해제
+			.and()
+				.formLogin()
+				.loginPage("/member/login")
+				.defaultSuccessUrl("/member/loginResult")
+				.failureUrl("/member/loginResult")
+				.loginProcessingUrl("/member/login")
+				.usernameParameter("memberId") //여기에 입력한 이름으로 input의 아이디와 이름을 설정할 수 있음
+				.passwordParameter("memberPw")
+			.and()
+				.exceptionHandling()
+				.accessDeniedPage("/member/accessDenied") //인증은 했지만 권한이 없어서 못 들어가는 페이지로 이동했을 때
+			.and()
+				.logout()
+				.invalidateHttpSession(true)
+				.logoutSuccessUrl("/item/list");
+				
+				
+		return security.build();
+	}
+	
+	@Bean //비밀번호 암호화 자동으로 해 주는 객체 생성
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	//security로 접근 권한을 설정하면 .js, .css 등 정적인 리소스에 접근도 권한을 체크
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().antMatchers("/js/**", "/css/**", "/images/**");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- src/main/java/kh/study/team2/shop/member/controller/MemberContorller.java
+++ src/main/java/kh/study/team2/shop/member/controller/MemberContorller.java
@@ -4,6 +4,8 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,6 +26,10 @@ public class MemberContorller {
 	@Resource(name="memberService") 
 	private MemberService memberService;
 	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	
 	@GetMapping("/join")
 	public String join(MemberVO memberVO) {
 		return "content/member/join";
@@ -31,31 +37,45 @@ public class MemberContorller {
 	
 	@PostMapping("/join")
 	public String doJoin(@Valid MemberVO memberVO, BindingResult bindingResult, Model model){
+
+		//비밀번호 암호화
+		String pw = encoder.encode(memberVO.getMemberPw()); //memberVO 안에서 input으로 입력받은 비밀번호를 가지고 와서 암호화 후 이름을 pw로 지정
+		memberVO.setMemberPw(pw); //암호화한 값 pw를 memberVO의 비밀번호로 세팅
 		
 		if(bindingResult.hasErrors()) {
 			System.out.println("error!");
 			return "content/join";
 		}
 		memberService.join(memberVO);
-		return "redirect:/item/list";
 		
+		return "redirect:/item/list";
 	}
+	
+	
 	@GetMapping("/login")
 	public String login(MemberVO memberVO) {
 		return "content/member/login";
 	}
+
+//스프링 시큐리티 사용하는 경우에는 필요 X
+//	@ResponseBody
+//	@PostMapping("/ajaxLogin")
+//	public boolean ajaxLogin(MemberVO memberVO, HttpSession session) {
+//		MemberVO loginInfo = memberService.login(memberVO);
+//		
+//		if(loginInfo != null) { 
+//			session.setAttribute("loginInfo", loginInfo);
+//		}
+//		return loginInfo == null ? false : true;
+//	}
 	
-	@ResponseBody
-	@PostMapping("/ajaxLogin")
-	public boolean ajaxLogin(MemberVO memberVO, HttpSession session) {
-		MemberVO loginInfo = memberService.login(memberVO);
+	//로그인 시도하면 login_result.html로 보내 준다
+	@GetMapping("/loginResult")
+	public String loginResult() {	
 		
-		if(loginInfo != null) { 
-			session.setAttribute("loginInfo", loginInfo);
-		}
-		return loginInfo == null ? false : true;
+		return "content/member/login_result";
 	}
-	
+
 	@ResponseBody
 	@PostMapping("/checkDuplId")
 	public boolean ID_Check(String memberId) {
