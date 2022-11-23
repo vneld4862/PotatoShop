d11c70c5afc7de39fccf323dc8cee702706cb970
jinyeee <wls9546@naver.com>
2022-11-23 13:51:44 +0900

    상품관리에서 전체선택 기능 구현

--- src/main/resources/static/js/manage/item_manage.js
+++ src/main/resources/static/js/manage/item_manage.js
@@ -1,2 +1,30 @@
 //alert("상품관리 화면연결~");
+function checkAll() {
+   if($("#AllCbox").is(':checked')) {
+      $("input[name=checkboxes]").prop("checked", true);
+   } else {
+      $("input[name=checkboxes]").prop("checked", false);
+   }
+}
+
+$(document).on("click", "input:checkbox[name=checkboxes]", function(e) {
+   
+   var chks = document.getElementsByName("checkboxes");
+   var chksChecked = 0;
+   
+   for(var i=0; i<chks.length; i++) {
+      var cbox = chks[i];
+      
+      if(cbox.checked) {
+         chksChecked++;
+      }
+   }
+   
+   if(chks.length == chksChecked){
+      $("#AllCbox").prop("checked", true);
+   }else{
+      $("#AllCbox").prop("checked",false);
+   }
+   
+});
 
