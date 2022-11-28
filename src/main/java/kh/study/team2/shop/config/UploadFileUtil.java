373c8c89ee10241265806ac18a8bc4498849be41
vneld4862 <vneld4862@gmail.com>
2022-11-28 18:26:56 +0900

    메인 화면 디자인 조금,검색 디자인 조금

--- src/main/java/kh/study/team2/shop/config/UploadFileUtil.java
+++ src/main/java/kh/study/team2/shop/config/UploadFileUtil.java
@@ -15,7 +15,7 @@ public class UploadFileUtil {
 	//파일이 첨부될 경로
 
 	//예진 경로
-	private static final String UPLOAD_PATH = "D:\\workspaceSTS\\cloneSpringTest\\PotatoShop\\src\\main\\resources\\static\\images\\";
+//	private static final String UPLOAD_PATH = "D:\\workspaceSTS\\cloneSpringTest\\PotatoShop\\src\\main\\resources\\static\\images\\";
 	//예진(노트북) 경로
 //	private static final String UPLOAD_PATH = "C:\\dev\\workspaceSTS\\cloneSpringTest\\PotatoShop\\src\\main\\resources\\static\\images\\";
 	//연지 경로
@@ -23,7 +23,7 @@ public class UploadFileUtil {
 //	private static final String UPLOAD_PATH = "D:\\dev\\workspaceSTS\\project\\PotatoShop\\src\\main\\resources\\static\\images\\";
 //	private static final String UPLOAD_PATH = "/Users/yz/git/PotatoShop/src/main/resources/static/images/";
 	//명현 경로
-//	private static final String UPLOAD_PATH = "C:\\workspace\\PotatoShop\\src\\main\\resources\\static\\images\\";
+	private static final String UPLOAD_PATH = "C:\\workspace\\PotatoShop\\src\\main\\resources\\static\\images\\";
 	//민하 경로
 //	private static final String UPLOAD_PATH = "C:\\Users\\202-30\\git\\PotatoShop\\src\\main\\resources\\static\\images\\";
 	
