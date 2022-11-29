189bf6174b345f7b411d8f9ca30d2a4766d52cf9
jinyeee <wls9546@naver.com>
2022-11-29 17:39:59 +0900

    이미지 미리보기 및 업데이트 기능 구현 중

--- src/main/resources/static/js/manage/update_item.js
+++ src/main/resources/static/js/manage/update_item.js
@@ -1,5 +1,4 @@
-alert('이미지 수정 해보기');
-
+//이미지 삭제 버튼 클릭 시(서브 이미지 삭제)
 function deleteImg(deleteBtn){
 	alert('이미지 삭제 구현하기');
 	const imgCode = deleteBtn.dataset.imgCode;
@@ -11,7 +10,35 @@ function deleteImg(deleteBtn){
 	    type: 'post',
 	    data:{'imgCode':imgCode, 'itemCode':itemCode}, //필요한 데이터
 	    success: function(itemInfo) {
-	      alert('이미지삭제성공');
+	      alert('이미지 삭제 성공');
+	      alert(itemInfo.itemCode);
+	      /*여기서 그림 다시 그려주기 구현...*/
+	     
+	      
+	      $(deleteBtn).closest('div').remove(); //j쿼리문법 **** 
+	      
+	    },
+	    error: function(){
+	       alert('실패');
+	    }
+	});
+	//ajax end
+	
+}
+
+//메인 이미지 삭제
+function deleteMainImg(){
+	alert('이미지 삭제 구현하기');
+	const imgCode = deleteBtn.dataset.imgCode;
+	const itemCode = deleteBtn.dataset.itemCode;
+	alert(itemCode);
+	//ajax start
+	$.ajax({
+	   url: '/manage/deleteImg', //요청경로
+	    type: 'post',
+	    data:{'imgCode':imgCode, 'itemCode':itemCode}, //필요한 데이터
+	    success: function(itemInfo) {
+	      alert('이미지 삭제 성공');
 	      alert(itemInfo.itemCode);
 	      /*여기서 그림 다시 그려주기 구현...*/
 	     
@@ -29,37 +56,19 @@ function deleteImg(deleteBtn){
 
 
 //메인 이미지 수정
-/*function readURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      document.getElementById('preview').src = e.target.result;
-    };
-    reader.readAsDataURL(input.files[0]);
-  } else {
-    document.getElementById('preview').src = "";
-  }
-}*/
-
-
-//메인 이미지 수정
 function readURL(input) {
-	$('.mainImgDiv').remove();
   if (input.files && input.files[0]) {
+	//원본이미지를 db에서 삭제
+       //  deleteImg(deleteBtn);이렇게 함수를 적어서 실행시켜주면 된다.
+
     var reader = new FileReader();
     reader.onload = function(e) {
-		let str = '';
-		str += '<div class="col-3">';
-		str += `<img src=${e.target.result} width="230px;" height="230px;">`;
-		str += '</div>';
-		$('.mainImgDiv').append(str);
-	
-	
-     // document.getElementById('preview').src = e.target.result;
+		document.getElementById('preview').src = e.target.result;
     };
     reader.readAsDataURL(input.files[0]);
+    
   } else {
-    document.getElementById('preview').src = "";
+    //document.getElementById('preview').src = "";
   }
 }
 
@@ -86,7 +95,7 @@ function readURL2(input) {
 			reader.readAsDataURL(img_file);
 		} else {
 			//document.getElementById('preview').src = "";
-			document.querySelectorAll('.preview').src = "";
+			//document.querySelectorAll('.preview').src = "";
 		}
 	}
 }
