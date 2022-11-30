e939e6be72b74263f22672a8bdbe5acf52010c4e
vneld4862 <vneld4862@gmail.com>
2022-11-30 13:55:01 +0900

    security 로그인시 정적리소스로 인한 에러검출 무시 추가

--- src/main/java/kh/study/team2/shop/config/SecurityConfig.java
+++ src/main/java/kh/study/team2/shop/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
 							, "/member/searchIdResult"
 							, "/notice/list").permitAll() //회원가입, 로그인, 게시글 목록, 게시글 상세보기, 아이디/비밀번호 찾기
 				.antMatchers("/admin/**").hasRole("ADMIN")
+				.antMatchers("/favicon.ico", "/resources/**", "/error").permitAll()
 				.anyRequest().authenticated() //로그인한 유저는 접근 허용
 				//제일 마지막에 권한 해제
 			.and()
